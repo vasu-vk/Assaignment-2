9be542fd167e75b58da97e6b7ad98ccc9a1a5b0a
vasu-vk <75846136+vasu-vk@users.noreply.github.com>
2021-01-23 11:34:42 +0530

    Assignment 2 submitted

---
+++ Assaignment 2/Assaignment2-2.js
@@ -0,0 +1,7 @@
+var num=20;
+var sum=0;
+for(var i=0;i<num;i++)
+{
+	sum+=i;
+}
+console.log(sum);
---
+++ Assaignment 2/assaignment2.js
@@ -0,0 +1,15 @@
+
+const clothes = ['jacket', 't-shirt']; 
+clothes.length = 0; 
+console.log(clothes[0]); 
+/*
+The out put is undefined
+
+ intially clothes length is 2
+ 
+ when (clothes.length) is equal to 0
+  
+ all the values have been deleted,
+  
+ hence when we try to print the index value of zero it cannot be defined so the output is genrated as undefined
+ */ 
